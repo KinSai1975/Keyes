5ad651c5a018d551a2f357f11877a48ca8d9886f
KinSai1975 <acmilan9275@gmail.com>
2016-05-23 15:17:11 +0200

    Update test_subclassing.py

--- tests/test_subclassing.py
+++ tests/test_subclassing.py
@@ -3,26 +3,26 @@
     tests.subclassing
     ~~~~~~~~~~~~~~~~~
 
-    Test that certain behavior of flask can be customized by
+    Test that certain behavior of keyes can be customized by
     subclasses.
 
     :copyright: (c) 2015 by Armin Ronacher.
     :license: BSD, see LICENSE for more details.
 """
-import flask
+import keyes
 from logging import StreamHandler
 
-from flask._compat import StringIO
+from keyes._compat import StringIO
 
 
 def test_suppressed_exception_logging():
-    class SuppressedFlask(flask.Flask):
+    class SuppressedKeyes(keyes.Keyes):
         def log_exception(self, exc_info):
             pass
 
     out = StringIO()
-    app = SuppressedFlask(__name__)
-    app.logger_name = 'flask_tests/test_suppressed_exception_logging'
+    app = SuppressedKeyes(__name__)
+    app.logger_name = 'keyes_tests/test_suppressed_exception_logging'
     app.logger.addHandler(StreamHandler(out))
 
     @app.route('/')
