5b0f630136f615be51dcc6a062efc22ea96885bf
KinSai1975 <acmilan9275@gmail.com>
2016-05-23 19:37:35 +0200

    Update simple_page.py

--- examples/blueprintexample/simple_page/simple_page.py
+++ examples/blueprintexample/simple_page/simple_page.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from keyes import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
 simple_page = Blueprint('simple_page', __name__,
