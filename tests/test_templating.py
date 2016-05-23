86262a82e3603a384bba2503362e52529241e170
KinSai1975 <acmilan9275@gmail.com>
2016-05-23 15:16:37 +0200

    Update test_templating.py

--- tests/test_templating.py
+++ tests/test_templating.py
@@ -11,50 +11,50 @@
 
 import pytest
 
-import flask
+import keyes
 import logging
 from jinja2 import TemplateNotFound
 
 
 def test_context_processing():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.context_processor
     def context_processor():
         return {'injected_value': 42}
     @app.route('/')
     def index():
-        return flask.render_template('context_template.html', value=23)
+        return keyes.render_template('context_template.html', value=23)
     rv = app.test_client().get('/')
     assert rv.data == b'<p>23|42'
 
 def test_original_win():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.route('/')
     def index():
-        return flask.render_template_string('{{ config }}', config=42)
+        return keyes.render_template_string('{{ config }}', config=42)
     rv = app.test_client().get('/')
     assert rv.data == b'42'
 
 def test_request_less_rendering():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     app.config['WORLD_NAME'] = 'Special World'
     @app.context_processor
     def context_processor():
         return dict(foo=42)
 
     with app.app_context():
-        rv = flask.render_template_string('Hello {{ config.WORLD_NAME }} '
+        rv = keyes.render_template_string('Hello {{ config.WORLD_NAME }} '
                                           '{{ foo }}')
         assert rv == 'Hello Special World 42'
 
 def test_standard_context():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     app.secret_key = 'development key'
     @app.route('/')
     def index():
-        flask.g.foo = 23
-        flask.session['test'] = 'aha'
-        return flask.render_template_string('''
+        keyes.g.foo = 23
+        keyes.session['test'] = 'aha'
+        return keyes.render_template_string('''
             {{ request.args.foo }}
             {{ g.foo }}
             {{ config.DEBUG }}
@@ -65,11 +65,11 @@ def test_standard_context():
 
 def test_escaping():
     text = '<p>Hello World!'
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.route('/')
     def index():
-        return flask.render_template('escaping_template.html', text=text,
-                                     html=flask.Markup(text))
+        return keyes.render_template('escaping_template.html', text=text,
+                                     html=keyes.Markup(text))
     lines = app.test_client().get('/').data.splitlines()
     assert lines == [
         b'&lt;p&gt;Hello World!',
@@ -82,11 +82,11 @@ def test_escaping():
 
 def test_no_escaping():
     text = '<p>Hello World!'
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.route('/')
     def index():
-        return flask.render_template('non_escaping_template.txt', text=text,
-                                     html=flask.Markup(text))
+        return keyes.render_template('non_escaping_template.txt', text=text,
+                                     html=keyes.Markup(text))
     lines = app.test_client().get('/').data.splitlines()
     assert lines == [
         b'<p>Hello World!',
@@ -100,21 +100,21 @@ def test_no_escaping():
     ]
 
 def test_escaping_without_template_filename():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     with app.test_request_context():
-        assert flask.render_template_string(
+        assert keyes.render_template_string(
             '{{ foo }}', foo='<test>') == '&lt;test&gt;'
-        assert flask.render_template('mail.txt', foo='<test>') == \
+        assert keyes.render_template('mail.txt', foo='<test>') == \
             '<test> Mail'
 
 def test_macros():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     with app.test_request_context():
-        macro = flask.get_template_attribute('_macro.html', 'hello')
+        macro = keyes.get_template_attribute('_macro.html', 'hello')
         assert macro('World') == 'Hello World!'
 
 def test_template_filter():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.template_filter()
     def my_reverse(s):
         return s[::-1]
@@ -123,7 +123,7 @@ def test_template_filter():
     assert app.jinja_env.filters['my_reverse']('abcd') == 'dcba'
 
 def test_add_template_filter():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     def my_reverse(s):
         return s[::-1]
     app.add_template_filter(my_reverse)
@@ -132,7 +132,7 @@ def test_add_template_filter():
     assert app.jinja_env.filters['my_reverse']('abcd') == 'dcba'
 
 def test_template_filter_with_name():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.template_filter('strrev')
     def my_reverse(s):
         return s[::-1]
@@ -141,7 +141,7 @@ def test_template_filter_with_name():
     assert app.jinja_env.filters['strrev']('abcd') == 'dcba'
 
 def test_add_template_filter_with_name():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     def my_reverse(s):
         return s[::-1]
     app.add_template_filter(my_reverse, 'strrev')
@@ -150,51 +150,51 @@ def test_add_template_filter_with_name():
     assert app.jinja_env.filters['strrev']('abcd') == 'dcba'
 
 def test_template_filter_with_template():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.template_filter()
     def super_reverse(s):
         return s[::-1]
     @app.route('/')
     def index():
-        return flask.render_template('template_filter.html', value='abcd')
+        return keyes.render_template('template_filter.html', value='abcd')
     rv = app.test_client().get('/')
     assert rv.data == b'dcba'
 
 def test_add_template_filter_with_template():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     def super_reverse(s):
         return s[::-1]
     app.add_template_filter(super_reverse)
     @app.route('/')
     def index():
-        return flask.render_template('template_filter.html', value='abcd')
+        return keyes.render_template('template_filter.html', value='abcd')
     rv = app.test_client().get('/')
     assert rv.data == b'dcba'
 
 def test_template_filter_with_name_and_template():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.template_filter('super_reverse')
     def my_reverse(s):
         return s[::-1]
     @app.route('/')
     def index():
-        return flask.render_template('template_filter.html', value='abcd')
+        return keyes.render_template('template_filter.html', value='abcd')
     rv = app.test_client().get('/')
     assert rv.data == b'dcba'
 
 def test_add_template_filter_with_name_and_template():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     def my_reverse(s):
         return s[::-1]
     app.add_template_filter(my_reverse, 'super_reverse')
     @app.route('/')
     def index():
-        return flask.render_template('template_filter.html', value='abcd')
+        return keyes.render_template('template_filter.html', value='abcd')
     rv = app.test_client().get('/')
     assert rv.data == b'dcba'
 
 def test_template_test():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.template_test()
     def boolean(value):
         return isinstance(value, bool)
@@ -203,7 +203,7 @@ def test_template_test():
     assert app.jinja_env.tests['boolean'](False)
 
 def test_add_template_test():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     def boolean(value):
         return isinstance(value, bool)
     app.add_template_test(boolean)
@@ -212,7 +212,7 @@ def test_add_template_test():
     assert app.jinja_env.tests['boolean'](False)
 
 def test_template_test_with_name():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.template_test('boolean')
     def is_boolean(value):
         return isinstance(value, bool)
@@ -221,7 +221,7 @@ def test_template_test_with_name():
     assert app.jinja_env.tests['boolean'](False)
 
 def test_add_template_test_with_name():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     def is_boolean(value):
         return isinstance(value, bool)
     app.add_template_test(is_boolean, 'boolean')
@@ -230,51 +230,51 @@ def test_add_template_test_with_name():
     assert app.jinja_env.tests['boolean'](False)
 
 def test_template_test_with_template():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.template_test()
     def boolean(value):
         return isinstance(value, bool)
     @app.route('/')
     def index():
-        return flask.render_template('template_test.html', value=False)
+        return keyes.render_template('template_test.html', value=False)
     rv = app.test_client().get('/')
     assert b'Success!' in rv.data
 
 def test_add_template_test_with_template():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     def boolean(value):
         return isinstance(value, bool)
     app.add_template_test(boolean)
     @app.route('/')
     def index():
-        return flask.render_template('template_test.html', value=False)
+        return keyes.render_template('template_test.html', value=False)
     rv = app.test_client().get('/')
     assert b'Success!' in rv.data
 
 def test_template_test_with_name_and_template():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.template_test('boolean')
     def is_boolean(value):
         return isinstance(value, bool)
     @app.route('/')
     def index():
-        return flask.render_template('template_test.html', value=False)
+        return keyes.render_template('template_test.html', value=False)
     rv = app.test_client().get('/')
     assert b'Success!' in rv.data
 
 def test_add_template_test_with_name_and_template():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     def is_boolean(value):
         return isinstance(value, bool)
     app.add_template_test(is_boolean, 'boolean')
     @app.route('/')
     def index():
-        return flask.render_template('template_test.html', value=False)
+        return keyes.render_template('template_test.html', value=False)
     rv = app.test_client().get('/')
     assert b'Success!' in rv.data
 
 def test_add_template_global():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.template_global()
     def get_stuff():
         return 42
@@ -282,30 +282,30 @@ def test_add_template_global():
     assert app.jinja_env.globals['get_stuff'] == get_stuff
     assert app.jinja_env.globals['get_stuff'](), 42
     with app.app_context():
-        rv = flask.render_template_string('{{ get_stuff() }}')
+        rv = keyes.render_template_string('{{ get_stuff() }}')
         assert rv == '42'
 
 def test_custom_template_loader():
-    class MyFlask(flask.Flask):
+    class MyKeyes(keyes.Keyes):
         def create_global_jinja_loader(self):
             from jinja2 import DictLoader
             return DictLoader({'index.html': 'Hello Custom World!'})
-    app = MyFlask(__name__)
+    app = MyKeyes(__name__)
     @app.route('/')
     def index():
-        return flask.render_template('index.html')
+        return keyes.render_template('index.html')
     c = app.test_client()
     rv = c.get('/')
     assert rv.data == b'Hello Custom World!'
 
 def test_iterable_loader():
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     @app.context_processor
     def context_processor():
         return {'whiskey': 'Jameson'}
     @app.route('/')
     def index():
-        return flask.render_template(
+        return keyes.render_template(
             ['no_template.xml',  # should skip this one
             'simple_template.html',  # should render this
             'context_template.html'],
@@ -316,32 +316,32 @@ def test_iterable_loader():
 
 def test_templates_auto_reload():
     # debug is False, config option is None
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     assert app.debug is False
     assert app.config['TEMPLATES_AUTO_RELOAD'] is None
     assert app.jinja_env.auto_reload is False
     # debug is False, config option is False
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     app.config['TEMPLATES_AUTO_RELOAD'] = False
     assert app.debug is False
     assert app.jinja_env.auto_reload is False
     # debug is False, config option is True
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     assert app.debug is False
     assert app.jinja_env.auto_reload is True
     # debug is True, config option is None
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     app.config['DEBUG'] = True
     assert app.config['TEMPLATES_AUTO_RELOAD'] is None
     assert app.jinja_env.auto_reload is True
     # debug is True, config option is False
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     app.config['DEBUG'] = True
     app.config['TEMPLATES_AUTO_RELOAD'] = False
     assert app.jinja_env.auto_reload is False
     # debug is True, config option is True
-    app = flask.Flask(__name__)
+    app = keyes.Keyes(__name__)
     app.config['DEBUG'] = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     assert app.jinja_env.auto_reload is True
@@ -362,7 +362,7 @@ def test_template_loader_debugging(test_apps):
             assert 'Error: the template could not be found' in text
             assert ('looked up from an endpoint that belongs to '
                     'the blueprint "frontend"') in text
-            assert 'See http://flask.pocoo.org/docs/blueprints/#templates' in text
+            assert 'See http://keyes.pocoo.org/docs/blueprints/#templates' in text
 
     with app.test_client() as c:
         try:
@@ -382,11 +382,11 @@ def test_template_loader_debugging(test_apps):
     assert len(called) == 1
 
 def test_custom_jinja_env():
-    class CustomEnvironment(flask.templating.Environment):
+    class CustomEnvironment(keyes.templating.Environment):
         pass
 
-    class CustomFlask(flask.Flask):
+    class CustomKeyes(keyes.Keyes):
         jinja_environment = CustomEnvironment
 
-    app = CustomFlask(__name__)
+    app = CustomKeyes(__name__)
     assert isinstance(app.jinja_env, CustomEnvironment)
